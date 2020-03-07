bfe06706eb6ce4632fbdab844a4fa495cece7d25
tolu <teomotun@ncsu.edu>
2020-03-07 02:26:19 -0500

    Updated UFO-2

--- UFO-Level-2/static/js/app.js
+++ UFO-Level-2/static/js/app.js
@@ -85,59 +85,6 @@ function clickButton(term) {
 
   });
 
-  /* Stops it from refreshing page */
-  d3.select('form').on('submit', function () {
-
-    d3.event.preventDefault()
-
-    // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
-
-    // Get the value property of the input element
-    var inputValue = inputElement.property("value");
-
-    if (term == 'datetime') {
-      var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
-    }
-
-    else if (term == 'city') {
-      var filteredData = tableData.filter(tableData => tableData.city === inputValue);
-    }
-
-    else if (term == 'state') {
-      var filteredData = tableData.filter(tableData => tableData.state === inputValue);
-    }
-
-    else if (term == 'country') {
-      var filteredData = tableData.filter(tableData => tableData.country === inputValue);
-    }
-
-    else if (term == 'shape') {
-      var filteredData = tableData.filter(tableData => tableData.shape === inputValue);
-    }
-
-    // Empty the table data before query
-    d3.select("tbody").html("");
-
-    filteredData.forEach((UFO_Report) => {
-      // Step 1: Loop Through `data` and Use d3 to append one table row `tr` for each weather report object
-      var row = tbody.append("tr");
-      // Step 2:  Use `Object.entries` to console.log each UFO report value
-
-      Object.entries(UFO_Report).forEach(([key, value]) => {
-        // Step 3: Use d3 to append 1 cell per UFO report value
-
-        var cell = row.append("td");
-        // Step 4: Use d3 to update each cell's text with
-
-        cell.text(value);
-
-      });
-
-    });
-
-  });
-
 }
   
 
@@ -177,15 +124,15 @@ areaSelect.addEventListener(`change`, (e) => {
     document.getElementById("term").placeholder = "cary";
   }
 
-  else if (term == 'State') {
+  else if (text == 'State') {
     document.getElementById("term").placeholder = "nc";
   }
 
-  else if (term == 'Country') {
+  else if (text == 'Country') {
     document.getElementById("term").placeholder = "us";
   }
 
-  else if (term == 'Shape') {
+  else if (text == 'Shape') {
     document.getElementById("term").placeholder = "circle";
   }
   
