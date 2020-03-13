1972bb5ad75edcc4528b0b4d70bcecd84bb34640
tolu <teomotun@ncsu.edu>
2020-03-13 05:32:55 -0400

    Updated app.js

--- static/js/app.js
+++ static/js/app.js
@@ -36,7 +36,9 @@ function init() {
 function clickButton(term) {
   
   button.on("click", function () {
-
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
     // Select the input element and get the raw HTML node
     var inputElement = d3.select("#term");
 
